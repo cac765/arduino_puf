e03411ab1468def0e68574650f492919179b5ac4
cac765 <cac765@nau.edu>
2021-04-17 10:32:41 -0600

    eval class creation

--- utils/eval_utils.py
+++ utils/eval_utils.py
@@ -10,17 +10,41 @@
 #
 ###############################################################################
 
-def calculate_hamming_distance(hex_key: str, hex_auth: str) -> int:
-    """Calculates the hamming distance between two hex strings."""
-    int_key = int(hex_key, 16)
-    int_auth = int(hex_auth, 16)
-    int_ham = int_key ^ int_auth
-    return bin(int_ham).count("1")
 
-def convert_hex_to_binary(hex_str: str) -> str:
-    """Converts a hex string to padded binary string."""
-    return bin(int(hex_str, 16))[2:].zfill(8)
+class Eval:
 
-def XOR(bin_1: str, bin_2: str) -> str:
-    """XOR two binary strings and return the result."""
-    pass
+    def __init__(self):
+        self.total_mismatch = {}
+
+    def calculate_hamming_distance(hex_key: str, hex_auth: str) -> int:
+        """Calculates the hamming distance between two hex strings."""
+        int_key = int(hex_key, 16)
+        int_auth = int(hex_auth, 16)
+        int_ham = int_key ^ int_auth
+        return bin(int_ham).count("1")
+
+    def convert_hex_to_binary(hex_str: str) -> str:
+        """Converts a hex string to padded binary string."""
+        return bin(int(hex_str, 16))[2:].zfill(8)
+
+    def track_single_mismatch(hex_1: str, hex_2: str) -> list:
+        """XOR two hex strings and track the locations of mismatched chars."""
+        # iterate through length of strings
+
+            # if chars are not equal
+
+                # add index to a list
+
+        # return list of indices of mismatched chars
+        pass
+
+    def track_total_mismatch(mismatches: list):
+        """Track the total occurrences of mismatches from the key."""
+        # iterate through list of single mismatch case
+
+            # check if the index exists as a key in the dictionary
+
+                # add one to the key's value
+
+            # first occurrence, create a new key with the value 1
+        pass
